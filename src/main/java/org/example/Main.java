c13bd2fdf160e69316034555cb1a57053dafd8ce
SergiyCh2023 <alien13579@ukr.net>
2024-06-19 23:16:33 +0300

    first commit without tests and others

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+/*
+1. git
+2. yml
+3. lambda
+4. sonarLint
+5.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+       Solution solution = new Solution();
+        System.out.println("Max area is: " + solution.findMaxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+
+    }
+}
+
+
